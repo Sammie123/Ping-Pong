0b30f68de6be4622f74598f5dd502bbb427810fd
lincoln <huylincoln007@yahoo.com>
2017-06-09 08:53:02 -0700

    Add JS content

--- Desktop/ping-pong/js/scripts.js
+++ Desktop/ping-pong/js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function) {
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
-    
-  }
-}
+    var pingPong = $("#userInput").val()
+    var num1= parseInt(pingPong);
+
+    for ( var index = 0; index <= num1 -1; index += 1) {
+      if (num1 % 3 = 0) {
+        var num1 = num1.replace("ping");
+      } else if (num1 % 5 = 0) {
+        var num1 = num1.replace("pong");
+      } else if (num1 % 3 = 0 && num1 % 5 = 0) {
+        var num1 = num1.replace("pingpong");
+      } else {
+        alert("hello");
+
+      $("#results").show();
+
+      });
+    });
+  });
+});
