04758159deadf53bca503ee129e84a54c8310d80
lincoln <huylincoln007@yahoo.com>
2017-06-09 08:58:14 -0700

    Fixing errors in JS

--- Desktop/ping-pong/js/scripts.js
+++ Desktop/ping-pong/js/scripts.js
@@ -1,22 +1,25 @@
-$(document).ready(function) {
+$(document).ready(function() {
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
     var pingPong = $("#userInput").val()
     var num1= parseInt(pingPong);
 
     for ( var index = 0; index <= num1 -1; index += 1) {
-      if (num1 % 3 = 0) {
+      if (num1 % 3 === 0) {
         var num1 = num1.replace("ping");
-      } else if (num1 % 5 = 0) {
+
+      } else if (num1 % 5 === 0) {
         var num1 = num1.replace("pong");
-      } else if (num1 % 3 = 0 && num1 % 5 = 0) {
+
+      } else if (num1 % 3 === 0 && num1 % 5 === 0) {
         var num1 = num1.replace("pingpong");
+
       } else {
-        alert("hello");
+        alert("You're doing it wrong");
 
-      $("#results").show();
-
-      });
-    });
+      };
+    };
+    
+    $("#results").show();
   });
 });
